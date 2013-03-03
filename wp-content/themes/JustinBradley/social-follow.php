25488e106e0314ede2c41c0eafed85d92e67e49d
brisull <bri_sull@yahoo.com>
2013-03-02 21:17:26 -0800

    New templates for social sharing/follow

---
+++ wp-content/themes/JustinBradley/social-follow.php
@@ -0,0 +1,29 @@
+<div class="social-follow">
+	<h2><em>Connect with Us</em></h2>
+	<p>Connect with JustinBradley for open jobs, career training, and more</p>
+	<ul>
+		<!-- LinkedIn -->
+		<li class="linkedin">
+			<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+			<script type="IN/FollowCompany" data-id="42595" data-counter="none"></script>
+		</li>
+		<!-- end LinkedIn -->
+		<!-- Twitter -->
+		<li class="twitter">
+		    <a href="https://twitter.com/JustinBradleyCo" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @JustinBradleyCo</a>
+		    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</li>
+		<!-- end Twitter -->
+		<!-- Facebook -->
+		<li class="facebook">
+			<div class="fb-follow" data-href="https://facebook.com/JustinBradleyCo" data-layout="button_count" data-show-faces="false"></div>
+		</li>
+		<!-- end facebook -->
+		
+	</ul>
+	<p>
+		<a name="Contact" href="/contact" class="button">
+			Contact
+		</a>
+	</p>
+</div>
---
+++ wp-content/themes/JustinBradley/social.php
@@ -0,0 +1,22 @@
+<div class="social-buttons">
+	<ul>
+		<!-- Twitter -->
+		<li class="twitter">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-related="jasoncosta" data-lang="en" data-count="none">Tweet</a>
+		    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</li>
+		<!-- end Twitter -->
+		<!-- Facebook -->
+		<li class="facebook">
+			<div class="fb-like" data-href="http://justinbradley.com" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false">
+			</div>
+		</li>
+		<!-- end facebook -->
+		<!-- LinkedIn -->
+		<li class="linkedin">
+			<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+			<script type="IN/Share"></script>
+		</li>
+		<!-- end LinkedIn -->
+	</ul>
+</div>
